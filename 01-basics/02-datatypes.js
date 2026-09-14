139acefd2d420630f18bfadd56502e8336ac79be
sagarx7-cmd <sagarsingh2038@gmail.com>
2026-09-14 19:31:54 +0000

    js datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+let name = "Sgaar"
+let age = 18
+let isloggedIn = true
+
+//number => 2 to the power of 53
+//null => standalone value
+//undefined =>
+// symbol => uniqueness
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
