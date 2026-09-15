572eb84a88250a341e44e5005efb8c3961e24295
sagarx7-cmd <sagarsingh2038@gmail.com>
2026-09-15 10:30:37 +0000

    Operations

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -53,3 +53,15 @@ let str3 = str1 + str2
 // console.log(1 + 2 + "2"); //32
 
 // console.log( (3 + 4) * 5 % 3);
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
