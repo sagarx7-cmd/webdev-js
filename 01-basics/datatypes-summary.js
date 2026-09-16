3273de5b9df6584e2df564ad7a43af03ef4a4379
sagarx7-cmd <sagarsingh2038@gmail.com>
2026-09-16 19:47:36 +0000

    Stack and Heap Memory

--- 01-basics/datatypes-summary.js
+++ 01-basics/datatypes-summary.js
@@ -34,3 +34,31 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++
+
+// There are 2 types of memory -
+// Stack Memory (Primitive) and Heap Memory (Reference)
+
+let myYoutubename = "sagardotcom"
+
+let anotherName = "myYoutubename"
+anotherName = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upid: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sagar@google.com"
+
+console.log(userOne);
+console.log(userTwo);
+
