9216f01f8c7babf73a276bb2d410e0477b30b53d
sagarx7-cmd <sagarsingh2038@gmail.com>
2026-09-16 12:14:56 +0000

    datatypes summary

--- 01-basics/datatypes-summary.js
+++ 01-basics/datatypes-summary.js
@@ -17,7 +17,20 @@ console.log(id === anotherId);
 // const bigNumber = 3456543576654356754n
 
 
-
 // Reference (Non primitive)
 
 // Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
