4cfc908dfe16eda4a5fd1c69a4ea523bcbe50aac
sagarx7-cmd <sagarsingh2038@gmail.com>
2026-09-22 11:05:43 +0000

    Functions 2

--- 03-basics/01-functions.js
+++ 03-basics/01-functions.js
@@ -41,3 +41,52 @@ function loginUserMessage(username = "sam"){ //"sam" is default value of usernam
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
+
+//Functions part 2
+
+// function calculateCartPrice(...num1){ //... is a rest operator here
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+//This will return us an array of arguments
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+//How to pass objects in functions
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+//Hum direct object bhi pass kar sakte hai in a function
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+// How to pass array in a function
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+//Hum directly array bhi pass kar sakte hai in a function
+
+console.log(returnSecondValue([200, 400, 500, 1000]));
