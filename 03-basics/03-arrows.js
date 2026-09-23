6f5a6c86b84240ef779211e4ad3b9f41d41c0554
sagarx7-cmd <sagarsingh2038@gmail.com>
2026-09-23 18:43:01 +0000

    Arrow functions in js

--- 03-basics/03-arrows.js
+++ 03-basics/03-arrows.js
@@ -18,3 +18,49 @@ const user = {
 // console.log(this); This will give us an empty object {}
 
 // If we execute line 18 on browser console we will get window object (global object)
+
+//ye jo mera context hai(this keyword) ise mai sirf object me use kar sakta hoon not in functions
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// Arrow Function - function keyword hatana hai and parenthesis ke baad arrow lagana hai, that's it
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+// chai()
+
+//Basic Arrow Function:
+
+//Explicit return arrow function : Basically jab hum return keyword use karte hai
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+//Implicit Return arrow function:
+//Agar round brackets use kar rhe hai toh no need to write return keyword
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+//The below code will return us an object
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
